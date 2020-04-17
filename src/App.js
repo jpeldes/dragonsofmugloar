862d7a3b4176e55f21e61e7d1d447d6626fd8d28
Jaak Peldes <jaakpeldes@gmail.com>
2020-04-17 14:36:43 +0300

    create GameContainer

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
-import React, { useState } from "react";
-import GameView from "./GameView";
+import React from "react";
 import "./App.css";
-import { GameList } from "./GameList";
+import GameContainer from "./GameContainer";
 
-const useLocalStorage = () => {
-  const gamesToStorage = (newSavedGames) =>
-    localStorage.setItem("games", JSON.stringify(newSavedGames));
-  const gamesFromStorage = () => {
-    try {
-      const str = localStorage.getItem("games");
-      return JSON.parse(str) || {};
-    } catch (e) {
-      return {};
-    }
-  };
-
-  return [gamesToStorage, gamesFromStorage];
-};
-
-function App() {
-  const [gamesToStorage, gamesFromStorage] = useLocalStorage();
-
-  const [games, setGames] = useState(gamesFromStorage());
-  const saveGame = (gameId, game) => {
-    let newSavedGames;
-    if (games.hasOwnProperty(gameId)) {
-      // Update existing game
-      newSavedGames = { ...games, [gameId]: game };
-    } else {
-      // Set new game as first
-      newSavedGames = { [gameId]: game, ...games };
-    }
-    setGames(newSavedGames);
-    gamesToStorage(newSavedGames);
-  };
-
-  const [activeGameId, setActiveGameId] = useState(null);
-  const activeGame = games[activeGameId];
-
+const App = () => {
   return (
     <div className="App">
-      {activeGameId ? (
-        <GameView game={activeGame} saveGame={saveGame} />
-      ) : (
-        <GameList
-          games={games}
-          saveGame={saveGame}
-          setActiveGameId={setActiveGameId}
-        />
-      )}
+      <GameContainer />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/GameContainer.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import GameView from "./GameView";
+import { GameList } from "./GameList";
+
+const useLocalStorage = () => {
+  const gamesToStorage = (newSavedGames) =>
+    localStorage.setItem("games", JSON.stringify(newSavedGames));
+  const gamesFromStorage = () => {
+    try {
+      const str = localStorage.getItem("games");
+      return JSON.parse(str) || {};
+    } catch (e) {
+      return {};
+    }
+  };
+
+  return [gamesToStorage, gamesFromStorage];
+};
+
+function GameContainer() {
+  const [gamesToStorage, gamesFromStorage] = useLocalStorage();
+
+  const [games, setGames] = useState(gamesFromStorage());
+  const saveGame = (gameId, game) => {
+    let newSavedGames;
+    if (games.hasOwnProperty(gameId)) {
+      // Update existing game
+      newSavedGames = { ...games, [gameId]: game };
+    } else {
+      // Set new game as first
+      newSavedGames = { [gameId]: game, ...games };
+    }
+    setGames(newSavedGames);
+    gamesToStorage(newSavedGames);
+  };
+
+  const [activeGameId, setActiveGameId] = useState(null);
+  const activeGame = games[activeGameId];
+
+  return (
+    <div className="GameContainer">
+      {activeGameId ? (
+        <GameView game={activeGame} saveGame={saveGame} />
+      ) : (
+        <GameList
+          games={games}
+          saveGame={saveGame}
+          setActiveGameId={setActiveGameId}
+        />
+      )}
+    </div>
+  );
+}
+
+export default GameContainer;
