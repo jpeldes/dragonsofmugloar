2f31c1243e0c5650d9e62bfe4a481720f089cadc
Jaak Peldes <jaakpeldes@gmail.com>
2020-04-17 14:25:50 +0300

    save games to localstorage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,36 @@ import GameView from "./GameView";
 import "./App.css";
 import { GameList } from "./GameList";
 
+const useLocalStorage = () => {
+  const gamesToStorage = (newSavedGames) =>
+    localStorage.setItem("games", JSON.stringify(newSavedGames));
+  const gamesFromStorage = () => {
+    try {
+      const str = localStorage.getItem("games");
+      return JSON.parse(str) || {};
+    } catch (e) {
+      return {};
+    }
+  };
+
+  return [gamesToStorage, gamesFromStorage];
+};
+
 function App() {
-  const [games, setGames] = useState({});
+  const [gamesToStorage, gamesFromStorage] = useLocalStorage();
+
+  const [games, setGames] = useState(gamesFromStorage());
   const saveGame = (gameId, game) => {
-    setGames({ ...games, [gameId]: game });
+    let newSavedGames;
+    if (games.hasOwnProperty(gameId)) {
+      // Update existing game
+      newSavedGames = { ...games, [gameId]: game };
+    } else {
+      // Set new game as first
+      newSavedGames = { [gameId]: game, ...games };
+    }
+    setGames(newSavedGames);
+    gamesToStorage(newSavedGames);
   };
 
   const [activeGameId, setActiveGameId] = useState(null);
